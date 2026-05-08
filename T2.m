cc34cd2e1dc03839f4a2cc5ba9bb6d94fb92bc1b
KylinKylinKylin <122366690+KylinKylinKylin@users.noreply.github.com>
2026-05-08 11:27:17 +0000

    Added some extra descriptions to the code

--- T2.m
+++ T2.m
@@ -7,9 +7,7 @@ p = trisphere(144, diameter);
 %plot( p, 'EdgeColor', 'b' ); hold on
 %plot( p, 'EdgeColor', 'b', 'nvec', 1 ); % show outer surface normals
 
-lambda = 900;
-k0 = 2*pi/lambda;
-
+% dielectric functions for gold and GaAs
 epsz = @(w) lorenze_drude(299792458*2*pi*6.58e-16/w, 'Au', 'LD');
 epsz_GaAs = @(w) GaAs_Adachi(299792458*2*pi*6.58e-16/w);
 
@@ -19,6 +17,7 @@ mat = [mat1, mat2];
 
 tau = BoundaryEdge(mat, p, [2,1]);
 
+% running the BEM solver
 bem = galerkin.bemsolver( tau, 'relcutoff', 2, 'waitbar', 1 );
 
 %%
@@ -29,31 +28,13 @@ pol = [0,0,1];
 
 exc = galerkin.planewave(pol, dir);
 
-% solve BEm equation
+% solve BEM equation
 lambda = 900; %setting wavelength of light in nm
 k0 = 2 * pi / lambda;
-sol = bem \ exc( tau, k0 );
+sol = bem \ exc( tau, k0 ); % This is the final e and h fields inside and outside the particle after the planewave excitation
 
 
-%% Don't need in the section
-% points
-n = 51;
-xx = 1 * diameter * linspace( -1, 1, n);
-[ x, y ] = ndgrid( xx, xx );
-pt = Point( tau, [ x( : ), y( : ), 0 * x( : ) ] );
-
-% size(pt) 2601 x 1
-
-% evaluate fields
-[e, h] = fields( sol, pt, 'relcutoff', 2, 'waitbar', 1); 
-[ei, hi] = fields(exc, pt, k0); % incident field
-
-
-ee_int = real( squeeze( dot( e, e, 2 ) ) );
-ee = real( squeeze( dot( ei, ei, 2 ) ) );
-%%
-
-% Compute S and D operators
+% Compute S and D operators from the existing code
 inout = 1;  % 1 = exterior, 2 = interior
 
 SD_ex = eval(bem.pot, k0, inout);
@@ -66,18 +47,18 @@ inout = 2;  % 1 = exterior, 2 = interior
 
 SD_in = eval(bem.pot, k0, inout);
 
-S1_in = SD_in.SL1;
-S2_in = SD_in.SL2;
+S1_in = SD_in.SL1; % times mu
+S2_in = SD_in.SL2; % times epsilon
 CC_in  = SD_in.DL;
 
 disp("Part1 done...")
-%% Making the projctors only for the electric fields
-% Projecting the external field inside
+
+%% Projecting the fields
 
 I1 = eye(size(CC_in, 1));
 s0 = zeros(size(CC_in));
 
-% full calderon matrix
+% full calderon matrix - new
 % tilde D [ f_E; f_H ]
 tildeD_ps = [0.5 * I1 - CC_ex, 1j * k0 * S1_ex; 0.5 * I1 - CC_ex, -1j * k0 * S1_ex];
 tildeD_mi = [0.5 * I1 + CC_in, -1j * k0 * S1_in; 0.5 * I1 + CC_in, 1j * k0 * S1_in];
@@ -94,15 +75,6 @@ f_h = f.h;
 
 f_0 = [f_e; f_h];
 
-% testing to see if D is orthogonal
-tol = 1e-10;
-cond = conj(tildeD_ps) * tildeD_mi;
-isOrthogonal = all(abs(cond(:)) < tol);
-
-
-if isOrthogonal == true
-    disp('True');
-end
 
 % (1 − Ds+ )i = ±(1 − Ds+ )f 0 ,
 rhs1 = (I2 - tildeD_ps) * f_0;
@@ -116,16 +88,22 @@ lhs2 = -(I2 - tildeD_mi) * tildeD_ps;
 
 g_s = lhs2 \ rhs2;
 
-writematrix(f_0, '~/BEM_1/Data/f_0.csv');
-
 disp('Part2 done...')
 
+%% Don't need in the section, this is for plotting
+% points
+n = 51;
+xx = 1 * diameter * linspace( -1, 1, n);
+[ x, y ] = ndgrid( xx, xx );
+pt = Point( tau, [ x( : ), y( : ), 0 * x( : ) ] );
+
+% size(pt) 2601 x 1
+
+% evaluate fields
+[e, h] = fields( sol, pt, 'relcutoff', 2, 'waitbar', 1); 
+[ei, hi] = fields(exc, pt, k0); % incident field
+
+
+ee_int = real( squeeze( dot( e, e, 2 ) ) );
+ee = real( squeeze( dot( ei, ei, 2 ) ) );
 %%
-
-%Make calderon matrix
-% write it out first!!!
-% I = sum |l><l|
-% for matrix mul between D_s, D_i
-% otherwise I = sum |l'><l|
-
-% sum f_v' (x') f_v(x) verify where these can be from the exc edge vertices
